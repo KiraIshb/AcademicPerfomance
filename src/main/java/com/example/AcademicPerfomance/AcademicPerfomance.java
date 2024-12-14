27465eab6ed5e04f54b2523445561607ed17c79f
KiraIshb <163666090+KiraIshb@users.noreply.github.com>
2024-12-14 19:57:48 +0500

    Update and rename App.java to AcademicPerfomance.java

--- src/main/java/com/example/AcademicPerfomance/AcademicPerfomance.java
+++ src/main/java/com/example/AcademicPerfomance/AcademicPerfomance.java
@@ -3,7 +3,7 @@ import com.example.db.DBRepository;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class App {
+public class AcademicPerformance {
     public static void main(String[] args) {
         var path = "assets/basicprogramming_2.csv";
 	var data = new HashMap<String, Group>();
